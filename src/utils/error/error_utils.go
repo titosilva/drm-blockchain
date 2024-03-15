99d0e15c028d8786219022a15c81382e42bbaa47
titosilva <jt.mat@hotmail.com>
2024-03-15 19:15:23 -0300

    feat: adding debug files to gitignore + adding error tools

--- src/utils/error/error_utils.go
+++ src/utils/error/error_utils.go
@@ -9,7 +9,15 @@ func NewfWithInner(inner error, format string, vals ...any) error {
 	return errors.Join(Newf(format, vals...), inner)
 }
 
+func NewWithInner(inner error, msg string) error {
+	return errors.Join(New(msg), inner)
+}
+
 func Newf(format string, vals ...any) error {
-	str := fmt.Sprintf(format, vals...)
-	return errors.New(str)
+	msg := fmt.Sprintf(format, vals...)
+	return New(msg)
+}
+
+func New(msg string) error {
+	return errors.New(msg)
 }
