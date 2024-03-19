92eb76298ac4fac37f3e2d7828b028b91b1408a9
titosilva <jt.mat@hotmail.com>
2024-03-19 15:44:04 -0300

    feat: adding option to allow the injection of interfaces

--- src/di/di.go
+++ src/di/di.go
@@ -6,8 +6,17 @@ import (
 )
 
 type DIContext struct {
-	Instances map[reflect.Type]any
-	Factories map[reflect.Type]any
+	Instances          map[reflect.Type]any
+	Factories          map[reflect.Type]any
+	InterfaceInstances map[reflect.Type]any
+	InterfaceFactories map[reflect.Type]any
+}
+
+func NewContext() *DIContext {
+	ctx := new(DIContext)
+	ctx.Instances = make(map[reflect.Type]any)
+	ctx.Factories = make(map[reflect.Type]any)
+	return ctx
 }
 
 func ensureInitialized(provider *DIContext) {
@@ -15,8 +24,16 @@ func ensureInitialized(provider *DIContext) {
 		provider.Instances = make(map[reflect.Type]any)
 	}
 
+	if provider.InterfaceInstances == nil {
+		provider.InterfaceInstances = make(map[reflect.Type]any)
+	}
+
 	if provider.Factories == nil {
 		provider.Factories = make(map[reflect.Type]any)
+	}
+
+	if provider.InterfaceFactories == nil {
+		provider.InterfaceFactories = make(map[reflect.Type]any)
 	}
 }
 
@@ -26,32 +43,75 @@ func AddSingleton[T any](provider *DIContext, instance *T) {
 	provider.Instances[tType] = instance
 }
 
-func AddFactory[T any](provider *DIContext, factory func() *T) {
+func AddInterfaceSingleton[T any](provider *DIContext, instance T) {
+	ensureInitialized(provider)
+	tType := reflect.TypeOf([0]T{}).Elem()
+	provider.InterfaceInstances[tType] = instance
+}
+
+func AddFactory[T any](provider *DIContext, factory func(*DIContext) *T) {
 	ensureInitialized(provider)
 	tType := reflect.TypeOf([0]T{}).Elem()
 	provider.Factories[tType] = factory
 }
 
+func AddInterfaceFactory[T any](provider *DIContext, factory func(*DIContext) T) {
+	ensureInitialized(provider)
+	tType := reflect.TypeOf([0]T{}).Elem()
+	provider.InterfaceFactories[tType] = factory
+}
+
 func GetService[T any](provider *DIContext) *T {
 	ensureInitialized(provider)
 	tType := reflect.TypeOf([0]T{}).Elem()
-	serviceGeneric, ok := provider.Instances[tType]
 
-	if !ok {
-		serviceFactory, ok := provider.Factories[tType]
-
-		if !ok {
-			str := fmt.Sprintf("Service %s singleton or factory not found", tType.Name())
-			panic(str)
+	serviceGeneric, found := provider.Instances[tType]
+	if found {
+		if service, ok := serviceGeneric.(*T); ok {
+			return service
 		}
-
-		serviceGeneric = serviceFactory.(func() *T)()
 	}
 
-	service, ok := serviceGeneric.(*T)
-	if !ok {
-		str := fmt.Sprintf("Failed to convert service %s", tType.Name())
-		panic(str)
+	serviceFactory, found := provider.Factories[tType]
+	if found {
+		serviceGeneric = serviceFactory.(func(*DIContext) *T)(provider)
+		if service, ok := serviceGeneric.(*T); ok {
+			return service
+		}
 	}
-	return service
+
+	var panicMsg string
+	if !found {
+		panicMsg = fmt.Sprintf("Service %s singleton or factory not found", tType.Name())
+	} else {
+		panicMsg = fmt.Sprintf("Failed to convert service %s", tType.Name())
+	}
+	panic(panicMsg)
+}
+
+func GetInterfaceService[T any](provider *DIContext) T {
+	tType := reflect.TypeOf([0]T{}).Elem()
+
+	serviceGeneric, found := provider.InterfaceInstances[tType]
+	if found {
+		if service, ok := serviceGeneric.(T); ok {
+			return service
+		}
+	}
+
+	serviceFactory, found := provider.InterfaceFactories[tType]
+	if found {
+		serviceGeneric = serviceFactory.(func(*DIContext) T)(provider)
+		if service, ok := serviceGeneric.(T); ok {
+			return service
+		}
+	}
+
+	var panicMsg string
+	if !found {
+		panicMsg = fmt.Sprintf("Service %s singleton or factory not found", tType.Name())
+	} else {
+		panicMsg = fmt.Sprintf("Failed to convert service %s", tType.Name())
+	}
+	panic(panicMsg)
 }
--- src/di/di_test.go
+++ src/di/di_test.go
@@ -5,12 +5,25 @@ import (
 	"testing"
 )
 
+type STestInterface interface {
+	GetContent() int
+	SetContent(int)
+}
+
 type STest struct {
 	content int
 }
 
 type STest2 struct {
 	content int
+}
+
+func (s STest2) GetContent() int {
+	return s.content
+}
+
+func (s *STest2) SetContent(v int) {
+	s.content = v
 }
 
 func Test__DISingleton__ShouldReturn__SameInstanceAlways(t *testing.T) {
@@ -29,7 +42,7 @@ func Test__DISingleton__ShouldReturn__SameInstanceAlways(t *testing.T) {
 
 func Test__DIFactory__ShouldReturn__NewInstanceEveryTime(t *testing.T) {
 	ctx := new(di.DIContext)
-	di.AddFactory[STest2](ctx, func() *STest2 {
+	di.AddFactory[STest2](ctx, func(*di.DIContext) *STest2 {
 		return new(STest2)
 	})
 
@@ -44,6 +57,41 @@ func Test__DIFactory__ShouldReturn__NewInstanceEveryTime(t *testing.T) {
 	}
 
 	if st2.content != 1000 {
+		t.Error("st2 content has changed")
+	}
+}
+
+func Test__DIInterfaceSingleton__ShouldReturn__NewInstanceEveryTime(t *testing.T) {
+	ctx := new(di.DIContext)
+	di.AddInterfaceSingleton[STestInterface](ctx, new(STest2))
+
+	test := di.GetInterfaceService[STestInterface](ctx)
+	test.SetContent(212031)
+
+	test2 := di.GetInterfaceService[STestInterface](ctx)
+
+	if test.GetContent() != test2.GetContent() {
+		t.Error("Different instances provided for STest")
+	}
+}
+
+func Test__DIInterfaceFactory__ShouldReturn__NewInstanceEveryTime(t *testing.T) {
+	ctx := new(di.DIContext)
+	di.AddInterfaceFactory[STestInterface](ctx, func(*di.DIContext) STestInterface {
+		return new(STest2)
+	})
+
+	st2 := di.GetInterfaceService[STestInterface](ctx)
+	st2.SetContent(1000)
+
+	st3 := di.GetInterfaceService[STestInterface](ctx)
+	st3.SetContent(2000)
+
+	if st2.GetContent() == st3.GetContent() {
+		t.Error("Same instance provided for STest2")
+	}
+
+	if st2.GetContent() != 1000 {
 		t.Error("st2 content has changed")
 	}
 }
