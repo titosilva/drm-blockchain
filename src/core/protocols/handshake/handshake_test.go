cd64504e16084e44941befe2789dd86f544f52cf
titosilva <jt.mat@hotmail.com>
2024-10-19 18:55:57 -0300

    ref: creating docs folder

--- src/core/protocols/handshake/handshake_test.go
+++ src/core/protocols/handshake/handshake_test.go
@@ -1,9 +1,20 @@
 package handshake_test
 
 import (
+	"context"
+	"drm-blockchain/src/core/protocols/handshake"
+	"drm-blockchain/src/di"
 	"testing"
 )
 
 func Test__Hello(t *testing.T) {
+	ctx := context.TODO()
+	di := di.NewContext()
+	host, err := handshake.Open("localhost:8080", ctx, di)
 
+	if err != nil {
+		t.Error(err)
+	}
+
+	host.Greet("localhost:8081")
 }
